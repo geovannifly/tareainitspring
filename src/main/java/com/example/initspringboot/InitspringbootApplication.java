34753dba6872f0f3c837b6e9b7c5eba80ff78a54
GeovanniFly <geovanni.fly@gmail.com>
2024-09-12 17:57:49 -0600

    second commit

---
+++ src/main/java/com/example/initspringboot/InitspringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.initspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InitspringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InitspringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/initspringboot/InitspringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.initspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InitspringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
